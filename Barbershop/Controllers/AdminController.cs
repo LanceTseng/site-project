349e929c7c3af1d57a59f0b9f46e76b8b320f3cb
Lance Tseng <lancetseng4out@gmail.com>
2024-11-14 16:59:41 -0500

    feature admin dashboard

---
+++ Barbershop/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Barbershop.Controllers
+{
+    [Authorize(Roles = "ADMIN")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
