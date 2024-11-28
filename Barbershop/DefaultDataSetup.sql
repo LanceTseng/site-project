c33cf83cb46d220b7400ed6ed43bac0a5b3c6dc5
Lance Tseng <lancetseng4out@gmail.com>
2024-11-28 00:09:03 -0500

    add default sql file

---
+++ Barbershop/DefaultDataSetup.sql
@@ -0,0 +1,9 @@
+﻿insert into Users (Username, Password, Email, Phone) values("admin", "admin", "admin@admin.com", "12312312345");
+
+insert into Roles (RoleName) values("ADMIN");
+insert into Roles (RoleName) values("CUSTOMER");
+insert into Roles (RoleName) values("EMPLOYEE");
+
+insert into UserRoles(UserId, RoleId) values ((select u.UserId from Users u where u.Username = "admin"),(select r.RoleId from Roles r where r.RoleName= "ADMIN") );
+
+insert into [dbo].[Services] (ServiceName, Price) values ("Haircut", 20);
