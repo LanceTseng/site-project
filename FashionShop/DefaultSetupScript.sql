27cf0a4f4c1a440a6a853e5ba21c012cc71590ec
Lance Tseng <lancetseng4out@gmail.com>
2024-11-30 22:00:44 -0500

    add sql script

---
+++ FashionShop/DefaultSetupScript.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO Products (ProductName, ProductDescription, Price, ImageUrl) 
+VALUES 
+('Dress', 'Dress Description', 12, 'https://i.ebayimg.com/images/g/sTgAAOSwinJk3YuY/s-l1600.webp'),
+('Skirt', 'Skirt Description', 30, 'https://m.media-amazon.com/images/I/61fIzCQod0L._AC_SX569_.jpg');
+
+
+insert into Users(UserName, Password, Email, Role) values('admin','admin','admin@mail','Admin');
+
