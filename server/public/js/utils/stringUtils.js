32f57ae9b2b790b86518de2ff8d877d353f59ed6
Lance Tseng <lancetseng4out@gmail.com>
2025-02-11 11:48:51 -0500

    parent task maintenance

--- server/public/js/utils/stringUtils.js
+++ server/public/js/utils/stringUtils.js
@@ -1,3 +1,7 @@
 export function isEqualIgnoreCase(str1, str2) {
     return new RegExp(`^${str1}$`, "i").test(str2);
 }
+
+export function formatDate(date) {
+    return date ? new Date(date).toLocaleDateString() : "";
+  }
