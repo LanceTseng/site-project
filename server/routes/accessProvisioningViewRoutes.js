6359785759f3f308cd2080424b8be67b1d82bc99
Lance Tseng <lancetseng4out@gmail.com>
2025-02-28 18:51:27 -0500

    access provisioning

--- server/routes/accessProvisioningViewRoutes.js
+++ server/routes/accessProvisioningViewRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const AccessProvisioningViewController = require("../controllers/accessProvisioningViewController");
 
 // Get all access provisioning records
-router.get("/access-provsioning-view", AccessProvisioningViewController.getAll);
+router.get("/access-provisioning-view", AccessProvisioningViewController.getAll);
 
 // Get access provisioning by ID
-router.get("/access-provsioning-view/:id", AccessProvisioningViewController.getById);
+router.get("/access-provisioning-view/:id", AccessProvisioningViewController.getById);
 
 // Get access provisioning by Role ID
-router.get("/access-provsioning-view/role/:roleId", AccessProvisioningViewController.getByRoleId);
+router.get("/access-provisioning-view/role/:roleId", AccessProvisioningViewController.getByRoleId);
 
 module.exports = router;
