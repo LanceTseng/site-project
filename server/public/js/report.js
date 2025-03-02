9d9a59667805cc3087752bb75ae7cca45948283a
Lance Tseng <lancetseng4out@gmail.com>
2025-03-01 23:46:18 -0500

    access auth check

---
+++ server/public/js/report.js
@@ -0,0 +1,34 @@
+import { accessVerify } from "./utils/authVerify.js";
+
+$(document).ready(function () {
+  $(".menu-card").click(function () {
+    const cardTitle = $(this).find(".card-title").text().trim();
+    if (!accessVerify(cardTitle)) {
+        Swal.fire("Error", `Access Denied. Please contact IT.`, "error");
+      return;
+    }
+
+    const link = $(this).data("link");
+    if (link) {
+      window.location.href = link;
+    }
+  });
+
+    // Optional: Make sure the card links are properly clicked on 'Go to' button
+    $(".custom-btn").on("click", function (e) {
+        e.stopPropagation(); // Prevent the card click from being triggered
+        e.preventDefault();
+        // Get the card title of the clicked button's card
+        const cardTitle = $(this)
+          .closest(".card")
+          .find(".card-title")
+          .text()
+          .trim();
+        if (!accessVerify(cardTitle)) {
+            Swal.fire("Error", `Access Denied. Please contact IT.`, "error");
+          return;
+        }
+        const link = $(this).closest(".card").data("link");
+        window.location.href = link;
+      });
+});
