725c5e4ae79d04053edf6f5e4daa45da34fd3299
Lance Tseng <lancetseng4out@gmail.com>
2025-03-02 15:44:16 -0500

    ticket

---
+++ HRProject/server/public/js/services/ticketServices.js
@@ -0,0 +1,154 @@
+import axiosInstance from "/js/utils/axiosInstance.js"; // Absolute path for browser
+const baseURL = "api/";
+// Function to create a new task
+
+class View {
+  ticketHead = `${baseURL}ticket-head/`;
+  ticketDetail = `${baseURL}ticket-detail/`;
+  ticketView = `${baseURL}ticket-view/`
+}
+
+const view = new View();
+
+//ticket head
+export const getAllTicketHeads = async () => {
+    try {
+        const response = await axiosInstance.get(view.ticketHead);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching ticket heads:", error);
+        throw error;
+    }
+};
+
+export const getTicketHeadById = async (id) => {
+    try {
+        const response = await axiosInstance.get(`${view.ticketHead}${id}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error fetching ticket head with id ${id}:`, error);
+        throw error;
+    }
+};
+
+export const createTicketHead = async (data) => {
+    try {
+        const response = await axiosInstance.post(view.ticketHead, data);
+        return response.data;
+    } catch (error) {
+        console.error("Error creating ticket head:", error);
+        throw error;
+    }
+};
+
+export const updateTicketHead = async (id, data) => {
+    try {
+        const response = await axiosInstance.put(`${view.ticketHead}${id}`, data);
+        return response.data;
+    } catch (error) {
+        console.error(`Error updating ticket head with id ${id}:`, error);
+        throw error;
+    }
+};
+
+export const deleteTicketHead = async (id) => {
+    try {
+        const response = await axiosInstance.delete(`${view.ticketHead}${id}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error deleting ticket head with id ${id}:`, error);
+        throw error;
+    }
+};
+
+//ticket detail
+export const getAllTicketDetails = async () => {
+    try {
+        const response = await axiosInstance.get(view.ticketDetail);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching ticket details:", error);
+        throw error;
+    }
+};
+
+export const getTicketDetailById = async (id) => {
+    try {
+        const response = await axiosInstance.get(`${view.ticketDetail}${id}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error fetching ticket detail with id ${id}:`, error);
+        throw error;
+    }
+};
+
+export const createTicketDetail = async (data) => {
+    try {
+        const response = await axiosInstance.post(view.ticketDetail, data);
+        return response.data;
+    } catch (error) {
+        console.error("Error creating ticket detail:", error);
+        throw error;
+    }
+};
+
+export const updateTicketDetail = async (id, data) => {
+    try {
+        const response = await axiosInstance.put(`${view.ticketDetail}${id}`, data);
+        return response.data;
+    } catch (error) {
+        console.error(`Error updating ticket detail with id ${id}:`, error);
+        throw error;
+    }
+};
+
+export const deleteTicketDetail = async (id) => {
+    try {
+        const response = await axiosInstance.delete(`${view.ticketDetail}${id}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error deleting ticket detail with id ${id}:`, error);
+        throw error;
+    }
+};
+
+//ticket view
+export const getAllTickets = async () => {
+    try {
+        const response = await axiosInstance.get(view.ticketView);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching tickets:", error);
+        throw error;
+    }
+};
+ 
+export const getTicketsByCondition = async (condition) => {
+    try {
+        const response = await axiosInstance.get(`${view.ticketView}condition?`, { params: condition });
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching tickets by condition:", error);
+        throw error;
+    }
+};
+
+export const getAllTicketHeadView = async () => {
+    try {
+        const response = await axiosInstance.get(`${view.ticketView}head`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching tickets:", error);
+        throw error;
+    }
+};
+
+export const getTicketHeadViewByCondition = async (condition) => {
+    try {
+        const response = await axiosInstance.get(`${view.ticketView}head/condition?`, {params: condition});
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching tickets:", error);
+        throw error;
+    }
+};
--- HRProject/server/repositories/ticketViewRepository.js
+++ HRProject/server/repositories/ticketViewRepository.js
@@ -12,10 +12,74 @@ class TicketViewRepository {
       throw error;
     }
   }
-  async getTicketsByCondition(ticket_topic, department_id, status, user_id) {
+  async getTicketsByCondition(ticket_topic, department_id, status, user_id, head_id) {
     try {
       const rows = await db.query(
         `SELECT * FROM v_tickets
+             WHERE (:ticket_topic IS NULL OR ticket_topic LIKE :ticket_topic) 
+             AND (:ticket_department_id IS NULL OR ticket_department_id = :ticket_department_id) 
+             AND (:ticket_status_id IS NULL OR ticket_status_id = :ticket_status_id)
+             AND (:ticket_request_by_id IS NULL OR ticket_request_by_id = :ticket_request_by_id )
+             ANS (:ticket_id IS NULL OR ticket_id = :ticket_id)`,
+        {
+          replacements: {
+            ticket_topic: ticket_topic ? `%${ticket_topic}%` : null, // Ensures LIKE works
+            ticket_department_id: department_id ? department_id : null,
+            ticket_status_id: status ? status : null,
+            ticket_request_by_id: user_id ? user_id : null,
+            ticket_id: head_id ? head_id : null
+          },
+          type: QueryTypes.SELECT,
+        }
+      );
+      return rows;
+    } catch (error) {
+      console.error("Error fetching access provisioning records:", error);
+      throw error;
+    }
+  }
+
+  async getAllTicketHeadView() {
+    try {
+      return await db.query(`SELECT distinct ticket_id,
+                ticket_topic,
+                ticket_description,
+                ticket_department_id,
+                department_name,
+                ticket_status_id,
+                ticket_status_name,
+                ticket_request_date,
+                ticket_request_by_id,
+                ticket_request_by_name,
+                ticket_last_updated_date
+                FROM v_tickets`, {
+        type: QueryTypes.SELECT,
+      });
+    } catch (error) {
+      console.error("Error fetching all tickets:", error);
+      throw error;
+    }
+  }
+  async getTicketHeadViewByCondition(
+    ticket_topic,
+    department_id,
+    status,
+    user_id
+  ) {
+    try {
+      const rows = await db.query(
+        `SELECT distinct ticket_id,
+                ticket_topic,
+                ticket_description,
+                ticket_department_id,
+                department_name,
+                ticket_status_id,
+                ticket_status_name,
+                ticket_request_date,
+                ticket_request_by_id,
+                ticket_request_by_name,
+                ticket_last_updated_date
+                FROM v_tickets
              WHERE (:ticket_topic IS NULL OR ticket_topic LIKE :ticket_topic) 
              AND (:ticket_department_id IS NULL OR ticket_department_id = :ticket_department_id) 
              AND (:ticket_status_id IS NULL OR ticket_status_id = :ticket_status_id)
