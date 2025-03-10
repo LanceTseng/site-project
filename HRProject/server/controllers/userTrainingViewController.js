6598de70559fa1c0ccf64dd1013a2fa16f09c596
Lance Tseng <lancetseng4out@gmail.com>
2025-03-09 22:45:23 -0400

    user training

---
+++ HRProject/server/controllers/userTrainingViewController.js
@@ -0,0 +1,23 @@
+const UserTrainingViewRepository = require('../repositories/userTrainingViewRepository');
+const { logger } = require("../middlewares/loggerMiddleware"); // Import logger
+
+class UserTrainingViewController {
+    async getTrainingModuleViewByCondition(req, res) {
+        try {
+            const { training_name, user_name, department, status } = req.query;
+            const userTraining = await UserTrainingViewRepository.geTrainingModuleViewByCondition(
+                training_name,
+                user_name,
+                department,
+                status
+            );
+            res.status(200).json(userTraining);
+        } catch (error) {
+            console.error("Error fetching training module view by condition: ", error);
+            res.status(500).json({ error: "An error occurred while fetching the training module view." });
+            logger.error(error.message);
+        }
+    }
+}
+
+module.exports = new UserTrainingViewController();
---
+++ HRProject/server/repositories/userTrainingViewRepository.js
@@ -0,0 +1,44 @@
+const { QueryTypes } = require("sequelize");
+const db = require("../config/database"); // Ensure your database connection is imported
+
+class UserTrainingViewRepository {
+  async geTrainingModuleViewByCondition(
+    training_name,
+    user_name,
+    department,
+    status
+  ) {
+    try {
+      let query = "SELECT * FROM v_user_training WHERE 1=1";
+      const replacements = {};
+
+      if (training_name) {
+        query += " AND training_module_name LIKE :training_name";
+        replacements.training_name = `%${training_name}%`;
+      }
+      if (user_name) {
+        query += " AND user_name LIKE :user_name";
+        replacements.user_name = `%${training_name}%`;
+      }
+      if (department) {
+        query += " AND training_department_id = :department";
+        replacements.department = department;
+      }
+      if (status) {
+        query += " AND status = :status";
+        replacements.status = status;
+      }
+
+      const userTraining = await db.query(query, {
+        type: QueryTypes.SELECT,
+        replacements: replacements,
+      });
+      return userTraining;
+    } catch (error) {
+      console.error("Error fetching user payments by condition: ", error);
+      throw error;
+    }
+  }
+}
+
+module.exports = new UserTrainingViewRepository();
