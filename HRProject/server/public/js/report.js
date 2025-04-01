9d94f07cda81e7131caf2c3b8f9d5bf10e711cb5
Lance Tseng <lancetseng4out@gmail.com>
2025-04-01 13:09:54 -0400

    add css

--- HRProject/server/public/js/report.js
+++ HRProject/server/public/js/report.js
@@ -4,25 +4,25 @@ const cardData = [
   {
     title: "User Task",
     icon: "fa-tasks",
-    color: "text-primary",
+    color: "text-secondary",
     link: "/report-user-task",
   },
   {
     title: "Profile",
     icon: "fa-id-card",
-    color: "text-warning",
+    color: "text-secondary",
     link: "/report-profile",
   },
   {
     title: "Document",
     icon: "fa-file-alt",
-    color: "text-info",
+    color: "text-secondary",
     link: "document.html",
   },
   {
     title: "Training",
     icon: "fa-chalkboard-teacher",
-    color: "text-primary",
+    color: "text-secondary",
     link: "/report-training",
   },
   // {
@@ -34,13 +34,13 @@ const cardData = [
   {
     title: "Ticket Request",
     icon: "fa-ticket",
-    color: "text-info",
+    color: "text-secondary",
     link: "/report-ticket",
   },
   {
     title: "Payment",
     icon: "fa-money-bill-1-wave",
-    color: "text-success",
+    color: "text-secondary",
     link: "/report-payment",
   },
 ];
@@ -50,12 +50,12 @@ function renderCardMenu() {
 
   cardData.forEach((card) => {
     const cardHTML = `
-      <div class="col-md-4">
+      <div class="col-md-4 animate__animated animate__backInDown">
         <div class="card menu-card" data-link="${card.link}">
           <div class="card-body text-center">
             <i class="fa-solid ${card.icon} ${card.color} fa-3x mb-3"></i>
             <h5 class="card-title">${card.title}</h5>
-            <a href="#" class="btn btn-outline-primary custom-btn">Go to ${card.title}</a>
+            <a href="#" class="btn btn-outline-secondary custom-btn">Go to ${card.title}</a>
           </div>
         </div>
       </div>
